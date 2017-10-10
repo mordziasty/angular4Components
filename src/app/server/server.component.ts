f289fdff6bddf864e69f0dbecfcc52be940c7f96
Łukasz <you@example.com>
2017-10-10 16:09:07 +0200

    Angular4 - databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
+    serverServer = 40;
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }

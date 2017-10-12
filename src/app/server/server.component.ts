88c16218cd2fffb51d3b1c837657067c411dcf32
Łukasz <you@example.com>
2017-10-12 13:32:36 +0200

    Angular4 - Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,16 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId = 10;
     serverStatus = 'offline';
-    serverServer = 40;
+
+    constructor() {
+this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
